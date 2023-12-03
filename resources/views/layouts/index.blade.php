69a8aa9e2fd2155197949a5643c0120393d25f71
rizkyamalsyah <rizkyamalsyah5@gmail.com>
2023-12-03 23:27:08 +0700

    first

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,6 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('bootstrap-icons/bootstrap-icons.css') }}">
     
     @include("includes.auth.style")
     @yield('custom-head')
--- resources/views/pages/dashboard/data.blade.php
+++ resources/views/pages/dashboard/data.blade.php
@@ -36,40 +36,48 @@
             </div>
         </div>
         <div class="topik-list container-fluid w-100 d-flex justify-content-center my-5">
-            <div class="row row-cols-4 w-50">
-                <div class="col">
-                    <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
-                        <img src="{{ asset('images/main/assets/icons/pilot.png') }}" style="width:95%;" class="align-self-end">
-                    </div>
-                    <div class="title-topik text-center fw-bold fs-3">
-                        Kereta
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
-                        <img src="{{ asset('images/main/assets/icons/pariwisata.png') }}" style="width:95%;" class="align-self-end">
-                    </div>
-                    <div class="title-topik text-center fw-bold fs-3">
-                        Pariwisata
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
-                        <img src="{{ asset('images/main/assets/icons/sailor.png') }}" style="width:82%;" class="align-self-end">
-                    </div>
-                    <div class="title-topik text-center fw-bold fs-3">
-                        Pelabuhan
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
-                        <img src="{{ asset('images/main/assets/icons/health.png') }}" style="width:100%;" class="align-self-end">
-                    </div>
-                    <div class="title-topik text-center fw-bold fs-3">
-                        Kesehatan
-                    </div>
-                </div>
-            </div>
+          <div class="row row-cols-4 d-flex justify-content-evenly">
+            <div class="col">
+                <a href="{{ route('data-pages-detail') }}">
+                  <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
+                      <img src="{{ asset('images/main/assets/icons/pilot.png') }}" style="width:95%;" class="align-self-end">
+                  </div>
+                  <div class="title-topik text-center fw-bold fs-3">
+                      Kereta
+                  </div>
+                </a>
+              </div>
+              <div class="col">
+                <a href="{{ route('data-pages-detail') }}">
+                  <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
+                      <img src="{{ asset('images/main/assets/icons/pariwisata.png') }}" style="width:95%;" class="align-self-end">
+                  </div>
+                  <div class="title-topik text-center fw-bold fs-3">
+                      Pariwisata
+                  </div>
+                </a>
+              </div>
+              <div class="col">
+                <a href="{{ route('data-pages-detail') }}">
+                  <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
+                      <img src="{{ asset('images/main/assets/icons/sailor.png') }}" style="width:82%;" class="align-self-end">
+                  </div>
+                  <div class="title-topik text-center fw-bold fs-3">
+                      Pelabuhan
+                  </div>
+                </a>
+              </div>
+              <div class="col">
+                <a href="{{ route('data-pages-detail') }}">
+                  <div class="rounded-3 icon-topik bg-white border border-secondary d-flex justify-content-center" style="width:200px;height:200px; overflow:hidden;">
+                      <img src="{{ asset('images/main/assets/icons/health.png') }}" style="width:100%;" class="align-self-end">
+                  </div>
+                  <div class="title-topik text-center fw-bold fs-3">
+                      Kesehatan
+                  </div>
+                </a>
+              </div>
+          </div>
         </div>
     </div>
 
